64b82e7faf349159f8f2b6c220fa473ae0bf2b22
tejaravutla287 <tejaravutla287@gmail.com>
2026-06-09 13:27:46 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "remote" {
+    organization = "YOUR_TFC_ORGANIZATION_NAME"
+    workspaces {
+      name = "aws-eks-poc"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Filter available Availability Zones
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+# VPC Module
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name                 = "eks-poc-vpc"
+  cidr                 = "10.0.0.0/16"
+  azs                  = slice(data.aws_availability_zones.available.names, 0, 3)
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway  = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+# EKS Module
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    poc_nodes = {
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  enable_cluster_creator_admin_permissions = true
+}
